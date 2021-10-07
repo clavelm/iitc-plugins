297b69c704c8b2c2965aa02cfa53b81b0917a7ce
clavelm <math.clavel@protonmail.com>
2021-10-07 16:36:16 +0000

    Deploying to dist from @ clavelm/iitc-plugins@1a248610e31047560326ee1d2200baae2caead69 🚀

---
+++ comm-filter.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id             iitc-plugin-comm-filter@udnp
+// @author         udnp
+// @name           IITC plugin: COMM Filter
+// @category       COMM
+// @version        0.5.8
+// @description    COMM Filter
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://github.com/clavelm/iitc-plugins/raw/dist/comm-filter.meta.js
+// @downloadURL    https://github.com/clavelm/iitc-plugins/raw/dist/comm-filter.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
