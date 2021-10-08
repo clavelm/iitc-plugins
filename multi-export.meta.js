0535bf3774de53fa9976e64a99dc3d6dd39eb8c7
clavelm <math.clavel@protonmail.com>
2021-10-08 17:05:07 +0000

    Deploying to dist from @ clavelm/iitc-plugins@4ef9e6f773a115bce914f6521a813c9d48ae584f 🚀

---
+++ multi-export.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author          https://github.com/modkin/Ingress-IITC-Multi-Export
+// @id              iitc-plugin-portal-multi-export
+// @name            IITC plugin: IITC plugin: Portal Multi Export
+// @category        Misc
+// @version         0.11
+// @namespace       https://github.com/jonatkins/ingress-intel-total-conversion
+// @description     Export portals from bookmarks, current view or polygon
+// @updateURL      https://github.com/clavelm/iitc-plugins/raw/dist/multi-export.meta.js
+// @downloadURL    https://github.com/clavelm/iitc-plugins/raw/dist/multi-export.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
