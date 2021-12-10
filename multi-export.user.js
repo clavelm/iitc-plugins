d1a51e6b9f1ef4d285ea6a4d874cadc2c1b9a0cb
clavelm <math.clavel@protonmail.com>
2021-12-10 11:23:08 +0000

    Deploying to dist from @ clavelm/iitc-plugins@0c2e582f9520a674051225768fa4824697b45fcc 🚀

--- multi-export.user.js
+++ multi-export.user.js
@@ -19,7 +19,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'dw235';
-plugin_info.dateTimeVersion = '2021-12-10-111736';
+plugin_info.dateTimeVersion = '2021-12-10-112307';
 plugin_info.pluginId = 'multi-export';
 //END PLUGIN AUTHORS NOTE
 
