117b59d1387226b36b2ffefcb347f21d95863240
clavelm <math.clavel@protonmail.com>
2021-12-12 21:46:40 +0000

    Deploying to dist from @ clavelm/iitc-plugins@bb7fc91eee00ca524f923a0005b572c7cda05a82 🚀

---
+++ keysByCobra.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         xelio
+// @name           IITC plugin: Keys
+// @category       Misc
+// @version        0.4.0-cobra
+// @description    Allow manual entry of key counts for each portal. Use the 'keys-on-map' plugin to show the numbers on the map, and 'sync' to share between multiple browsers or desktop/mobile.
+// @id             keys
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://github.com/clavelm/iitc-plugins/raw/dist/keysByCobra.meta.js
+// @downloadURL    https://github.com/clavelm/iitc-plugins/raw/dist/keysByCobra.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
