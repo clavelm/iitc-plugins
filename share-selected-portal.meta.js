ce4d435bbe729503c44994aac5eee3a8771841bb
Mathieu CLAVEL <math.clavel@protonmail.com>
2021-12-13 23:35:56 +0100

    ssp: Re-add only the meta, but will be deleted next build…

---
+++ share-selected-portal.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         Mathieu CLAVEL
+// @name           IITC plugin: Share selected portal
+// @category       Controls
+// @version        0.2.0
+// @description    Add a share link when a portal is selected
+// @id             share-selected-portal
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://github.com/clavelm/iitc-plugin-share-selected-portal/releases/latest/download/share-selected-portal.meta.js
+// @downloadURL    https://github.com/clavelm/iitc-plugin-share-selected-portal/releases/latest/download/share-selected-portal.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
