bc5cbfe344f75a4330cb25284d7abf72b5a5d81d
Mathieu CLAVEL <math.clavel@protonmail.com>
2021-12-24 13:07:58 +0100

    ssp: Update version to 0.2.0

--- plugins/share-selected-portal.js
+++ plugins/share-selected-portal.js
@@ -1,7 +1,7 @@
 // @author         Mathieu CLAVEL
 // @name           Share selected portal
 // @category       Controls
-// @version        0.1.2
+// @version        0.2.0
 // @description    Add a share link when a portal is selected
 
 // use own namespace for plugin
